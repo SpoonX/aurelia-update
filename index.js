e3e5a2ab76506e990222e6a862b14a93d293aaf7
RWOverdijk <r.w.overdijk@gmail.com>
2015-11-11 17:25:22 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,53 @@
+var dependencies    = require(process.cwd() + '/package.json').jspm.dependencies,
+    dependencyNames = Object.getOwnPropertyNames(dependencies),
+    jspm            = require('jspm/api');
+
+function installNextDependency () {
+  if (!dependencyNames || 0 === dependencyNames.length) {
+    return console.log('> Finished.');
+  }
+
+  var dependencyName     = dependencyNames.pop(),
+      depencyInstallPath = dependencies[dependencyName].split('@')[0];
+
+  if (!dependencyName.match(/(^aurelia\-|.*?\/aurelia\-.*?$)/)) {
+    return installNextDependency();
+  }
+
+  notifyOnProgress(dependencyName);
+
+  jspm.install(dependencyName, depencyInstallPath, {parent: true})
+    .then(function () {
+      notifyOnProgress(dependencyName, true);
+      installNextDependency();
+    })
+    .catch(function (error) {
+      console.log(error);
+      notifyOnProgress(dependencyName, error.toString());
+      installNextDependency();
+    });
+}
+
+function notifyOnProgress (dependency, success) {
+  var message = '- ' + dependency + ': ';
+
+  process.stdout.clearLine();
+  process.stdout.cursorTo(0);
+
+  if (typeof success === 'undefined') {
+    message += 'Installing...';
+  } else if (true === success) {
+    message += 'Success!';
+  } else {
+    message += 'Failed! Message: ' + success.toString();
+  }
+  process.stdout.write(message);
+
+  if (true === success) {
+    process.stdout.write('\n');
+  }
+}
+
+console.log('> Starting update.');
+
+installNextDependency();
